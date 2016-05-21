92ef83a85b2e9edf9e17e216790afb8be4b820ef
Hyoga Yoshida <hyoga0216@gmail.com>
2016-05-22 00:02:17 +0900

    なんかいろいろできてきた

--- app/server.rb
+++ app/server.rb
@@ -1,9 +1,10 @@
 require 'em-websocket'
 require 'thin'
 require 'sinatra'
+require 'json'
+require './app/bot.rb'
 
 set :public_folder, './app'
-
 
 EM.run do
   class App < Sinatra::Application
@@ -11,7 +12,7 @@ EM.run do
       File.read('app/index.html')
     end
 
-    post "/" do
+    post "/bot" do
        return "hoge"
     end
 
@@ -20,6 +21,37 @@ EM.run do
   server = Thin::Server.start App, '0.0.0.0', 9000
 
 
+  connections = []
   EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 3000) do |ws|
+    ws.onopen do
+       connections << ws
+    end
+
+    ws.onmessage do |message|
+      connections.each do |conn|
+
+        if message.include?("bot") then
+           message = message.split(" ")
+           command = message[1]
+           data = message[2]
+
+           c = {
+             :"command" => message[1],
+             :"data" => message[2]
+           }
+
+           bot = Bot.new(c)
+           bot.generateHash()
+           message = bot.hash
+        end
+
+        message = {
+          "data"=> message
+        }
+
+         conn.send(message.to_json)
+      end
+    end
+
   end
 end  
